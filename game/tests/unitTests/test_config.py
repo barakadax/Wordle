db1f4f9cfd2daef440ef93564d300ce89aa811c9
barakadax <barakadax@gmail.com>
2024-02-04 18:49:23 +0200

    test are working now with __init__

--- game/tests/unitTests/test_config.py
+++ game/tests/unitTests/test_config.py
@@ -1,7 +1,6 @@
+import pytest
 from config import config
-from unittest import TestCase
 
-class ConfigUnitTests(TestCase):
-    def test_no_arguments_should_throw(self):
-        with self.assertRaises(ValueError):
-            config()
+def test_no_arguments_should_throw():
+    with pytest.raises(ValueError):
+        config()
